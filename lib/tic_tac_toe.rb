02427beea9fef85288e0e0d6b93d697937e30f43
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-18 17:28:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,8 +45,9 @@ class TicTacToe
 
   def turn
     puts "Please select a number between 1 and 9."
-    user_input = input_to_index(gets.chomp)
-    until valid_move?(user_input)
+
+    until valid_move?(user_input) do
+      user_input = input_to_index(gets.chomp)
       @board[user_input] = current_player
       display_board
 
