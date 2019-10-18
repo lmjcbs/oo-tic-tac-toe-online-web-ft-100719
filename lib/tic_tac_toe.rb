7f146a6a81198fd59a74e2e5fe6c325814c9d262
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-18 16:48:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,6 +16,7 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 
   end
 
